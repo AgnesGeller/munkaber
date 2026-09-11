f33ae1af2ca8f8f663c25c32730e00f0220ea1a4
AgnesGeller <agnesgeller@cyberanalys.com>
2026-09-11 16:59:22 +0200

    Add payroll statistics and daily fund accrual

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="diszkertek-munkaber-v30";
+const CACHE="diszkertek-munkaber-v31";
 const FILES=["./","index.html","css/style.css","js/supabase-config.js","js/storage.js","js/login.js","js/app.js","assets/diszkertek-logo.png","assets/forint-icon.png","manifest.webmanifest"];
-self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
-self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
+self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request))) });
