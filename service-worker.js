cf79c0f7e37774ee9c589f4c193e0141e4f31e3e
AgnesGeller <agnesgeller@cyberanalys.com>
2026-09-11 08:34:48 +0200

    Fix payroll cashbox calculations

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="diszkertek-munkaber-v21";
+const CACHE="diszkertek-munkaber-v22";
 const FILES=["./","index.html","css/style.css","js/supabase-config.js","js/storage.js","js/login.js","js/app.js","assets/diszkertek-logo.png","assets/forint-icon.png","manifest.webmanifest"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
